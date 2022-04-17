222d5b256afe9c2ac5a36384d29596cb94db0080
arora-saksham <90066687+arora-saksham@users.noreply.github.com>
2022-04-16 21:46:03 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+import App from './App';
+
+// Styles
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
